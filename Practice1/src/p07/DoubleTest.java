b0ebb696d8b2cf960fb5c8b91bd1589eef3e69a6
CodeDiary18 <codediary18@gmail.com>
2021-06-24 00:37:58 +0900

    practice7

---
+++ Practice1/src/p07/DoubleTest.java
@@ -0,0 +1,11 @@
+package p07;
+
+public class DoubleTest {
+
+	public static void main(String[] args) {
+		double dnum = 3.14;
+		float fnum = 3.14f;
+		System.out.println(dnum);
+		System.out.println(fnum);		
+	}
+}
