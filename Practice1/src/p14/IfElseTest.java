935d2f92dc9eb798bd2186275993db3c3c428807
CodeDiary18 <codediary18@gmail.com>
2021-06-26 00:56:33 +0900

    practice14

---
+++ Practice1/src/p14/IfElseTest.java
@@ -0,0 +1,14 @@
+package p14;
+import java.util.Scanner;
+
+public class IfElseTest {
+	public static void main(String[] args) {
+		int age = 7;
+		if(age>=8) {
+			System.out.println("학교에 다닌다!");
+		}
+		else {
+			System.out.println("학교에 다니지 않는다!");
+		}
+	}
+}
