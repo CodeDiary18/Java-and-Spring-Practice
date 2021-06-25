bb458164b3d8a8ff86e1c5aeee8bf602ebe5098d
CodeDiary18 <codediary18@gmail.com>
2021-06-26 00:37:24 +0900

    practice13

---
+++ Practice1/src/p13/BitTest.java
@@ -0,0 +1,20 @@
+package p13;
+
+public class BitTest {
+	public static void main(String[] args) {
+		int num1 = 5;	// 00000101
+		int num2 = 10;	// 00001010
+		
+		System.out.println(num1 | num2);
+		System.out.println(num1 & num2);
+		System.out.println(num1 ^ num2);
+		System.out.println(~num1);
+		
+		System.out.println(num1 << 2);
+		System.out.println(num1);
+		System.out.println(num1 <<= 2);
+		System.out.println(num1);
+		
+	}
+
+}
