8db673abc5b4fb3f4ae58488b1fd8c63a9674164
CodeDiary18 <codediary18@gmail.com>
2021-06-28 00:46:01 +0900

    practice18

---
+++ Practice1/src/p18/DoWhileTest.java
@@ -0,0 +1,16 @@
+package p18;
+import java.util.Scanner;
+
+public class DoWhileTest {
+	public static void main(String[] args) {
+		Scanner scanner = new Scanner(System.in);
+		int input;
+		int sum = 0;
+		do {
+			input = scanner.nextInt();
+			sum += input;
+		}while(input != 0);
+		
+		System.out.println(sum);
+	}
+}
