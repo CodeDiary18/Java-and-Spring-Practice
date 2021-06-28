21952d31d5c00b45ce17fcc362a0cc8af2498bdb
CodeDiary18 <codediary18@gmail.com>
2021-06-29 01:16:47 +0900

    practice2

---
+++ Practice2/src/p02/Order.java
@@ -0,0 +1,9 @@
+package p02;
+
+public class Order {
+	int orderId;
+	String buyerId;
+	String sellerId;
+	int productId;
+	String orderDate;
+}
---
+++ Practice2/src/p02/UserInfo.java
@@ -0,0 +1,9 @@
+package p02;
+
+public class UserInfo {
+	String userId;
+	String userPassWord;
+	String userName;
+	String userAddress;
+	int phoneNumber;
+}
