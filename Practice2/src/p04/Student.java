15b1566ae5823c935a695cb6595fc515320be2a1
CodeDiary18 <codediary18@gmail.com>
2021-06-30 01:59:52 +0900

    practice4

---
+++ Practice2/src/p04/Student.java
@@ -0,0 +1,15 @@
+package p04;
+
+public class Student {
+	public int studentID;
+	public String studentName;
+	public String address;
+	
+	public void showStudentInfo() {
+		System.out.println(studentName + ","+address);
+	}
+	
+	public String getStudentName() {
+		return studentName;
+	}
+}
---
+++ Practice2/src/p04/StudentTest.java
@@ -0,0 +1,20 @@
+package p04;
+
+public class StudentTest {
+	public static void main(String[] args) {
+		Student studentLee = new Student();
+		studentLee.studentName = "이순신";
+		studentLee.address = "서울";
+		
+		studentLee.showStudentInfo();
+		
+		Student studentKim = new Student();
+		studentKim.studentName = "김유신";
+		studentKim.address = "경주";
+		
+		studentLee.showStudentInfo();
+		
+		System.out.println(studentLee);
+		System.out.println(studentKim);
+	}
+}
