7feec0915fad07902d23baf18e205b809f499524
CodeDiary18 <codediary18@gmail.com>
2021-07-03 17:31:37 +0900

    practice19

---
+++ Practice2/src/p19/CarFactory.java
@@ -0,0 +1,16 @@
+package p19;
+
+public class CarFactory {
+	private static CarFactory instance = new CarFactory();
+	private CarFactory() {}
+	public static CarFactory getInstance() {
+		if(instance == null) {
+			instance = new CarFactory();
+		}
+		return instance;
+	}
+	public Car createCar() {
+		Car car = new Car();
+		return car;
+	}
+}
