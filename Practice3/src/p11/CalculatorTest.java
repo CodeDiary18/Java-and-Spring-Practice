89e743e755a8fb6d471b07d4d963d4e293aa6253
CodeDiary18 <codediary18@gmail.com>
2021-07-11 00:55:47 +0900

    practice11

---
+++ Practice3/src/p11/CalculatorTest.java
@@ -0,0 +1,18 @@
+package p11;
+
+public class CalculatorTest {
+	public static void main(String[] args) {
+		Calc calc = new CompleteCalc();
+		int num1 = 10;
+		int num2 = 5;
+		
+		System.out.println(num1 + "+" + num2 + "=" +
+				calc.add(num1, num2));
+		System.out.println(num1 + "-" + num2 + "=" +
+				calc.substract(num1, num2));
+		System.out.println(num1 + "*" + num2 + "=" +
+				calc.times(num1, num2));
+		System.out.println(num1 + "/" + num2 + "=" +
+				calc.divide(num1, num2));
+	}
+}
