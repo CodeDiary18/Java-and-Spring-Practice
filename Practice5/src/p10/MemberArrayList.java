289d50fcec815840537ef050e45ca4351b0109c6
CodeDiary18 <codediary18@gmail.com>
2021-07-19 05:38:59 +0900

    practice10

---
+++ Practice5/src/p10/MemberArrayList.java
@@ -0,0 +1,39 @@
+package p10;
+
+import java.util.ArrayList;
+
+public class MemberArrayList {
+	private ArrayList<Member> arrayList;
+	
+	public MemberArrayList() {
+		arrayList = new ArrayList<Member>();
+	}
+	
+	public MemberArrayList(int size) {
+		arrayList = new ArrayList<Member>(size);
+	}
+	
+	public void addMember(Member member) {
+		arrayList.add(member);
+	}
+	
+	public boolean removeMember(int memberId) {
+		for(int i =0; i<arrayList.size(); i++){
+			Member member = arrayList.get(i);
+			int tempId = member.getMemberId();
+			if(tempId == memberId){
+				arrayList.remove(i);
+				return true;
+			}
+		}
+		System.out.println(memberId + "가 존재하지 않습니다");
+		return false;
+	}
+	
+	public void showAllMember(){
+		for(Member member : arrayList){
+			System.out.println(member);
+		}
+		System.out.println();
+	}
+}
