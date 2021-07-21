5dba1711567040c4b32c398a58881f21ed6c76ff
CodeDiary18 <codediary18@gmail.com>
2021-07-22 04:01:23 +0900

    bye page add

--- src/main/java/com/example/firstproject/FirstprojectApplication.java
+++ src/main/java/com/example/firstproject/FirstprojectApplication.java
@@ -2,7 +2,6 @@ package com.example.firstproject;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.web.bind.annotation.GetMapping;
 
 @SpringBootApplication
 public class FirstprojectApplication {
--- src/main/java/com/example/firstproject/controller/FirstController.java
+++ src/main/java/com/example/firstproject/controller/FirstController.java
@@ -7,9 +7,17 @@ import org.springframework.web.bind.annotation.GetMapping;
 @Controller
 public class FirstController {
 
-@GetMapping("/hi")
+    @GetMapping("/hi")
     public String niceToMeetYou(Model model){
         model.addAttribute("username", "cd");
         return "greetings"; //templates/greetings.mustache -> 브라우저로 전송
     }
+
+    @GetMapping("bye")
+    public String seeYouNext(Model model){
+        model.addAttribute("nickname","홍길동");
+        return "goodbye";
+    }
 }
+
+
