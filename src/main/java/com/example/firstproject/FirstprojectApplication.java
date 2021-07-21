b800e59144ad01fcf4c0825fd435733d214813d8
CodeDiary18 <codediary18@gmail.com>
2021-07-22 00:12:31 +0900

    뷰 템플릿 - mustache

--- src/main/java/com/example/firstproject/FirstprojectApplication.java
+++ src/main/java/com/example/firstproject/FirstprojectApplication.java
@@ -2,6 +2,7 @@ package com.example.firstproject;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.web.bind.annotation.GetMapping;
 
 @SpringBootApplication
 public class FirstprojectApplication {
