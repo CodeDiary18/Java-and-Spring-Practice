cc5e9be2a27d35913817338b5e90c56ab7f97b35
CodeDiary18 <codediary18@gmail.com>
2021-07-23 03:23:41 +0900

    lombok으로 코드 리팩토링

--- src/main/java/com/example/firstproject/dto/ArticleForm.java
+++ src/main/java/com/example/firstproject/dto/ArticleForm.java
@@ -1,23 +1,14 @@
 package com.example.firstproject.dto;
 
 import com.example.firstproject.entity.Article;
+import lombok.AllArgsConstructor;
+import lombok.ToString;
 
+@AllArgsConstructor //생성자 대체
+@ToString   //toString() 대체
 public class ArticleForm {//폼 데이터를 받아올 그릇
     private String title;
     private String content;
-
-    public ArticleForm(String title, String content) {
-        this.title = title;
-        this.content = content;
-    }
-
-    @Override
-    public String toString() {
-        return "ArticleForm{" +
-                "title='" + title + '\'' +
-                ", content='" + content + '\'' +
-                '}';
-    }
 
     public Article toEntity() {
         return new Article(null,title,content);
