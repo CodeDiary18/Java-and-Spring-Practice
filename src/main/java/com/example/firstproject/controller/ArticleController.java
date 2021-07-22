0a4ec7f776c2ff950230344baf71962c744ab9d6
CodeDiary18 <codediary18@gmail.com>
2021-07-22 19:04:34 +0900

    폼 데이터 전송 & DTO

--- src/main/java/com/example/firstproject/controller/ArticleController.java
+++ src/main/java/com/example/firstproject/controller/ArticleController.java
@@ -1,7 +1,9 @@
 package com.example.firstproject.controller;
 
+import com.example.firstproject.dto.ArticleForm;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
 
 @Controller
 public class ArticleController {
@@ -9,5 +11,11 @@ public class ArticleController {
     @GetMapping("/articles/new")
     public String newArticleForm(){
         return "articles/new";
+    }
+
+    @PostMapping("/articles/create")
+    public String createArticle(ArticleForm form){
+        System.out.println(form.toString());
+        return "";
     }
 }
---
+++ src/main/java/com/example/firstproject/dto/ArticleForm.java
@@ -0,0 +1,19 @@
+package com.example.firstproject.dto;
+
+public class ArticleForm {//폼 데이터를 받아올 그릇
+    private String title;
+    private String content;
+
+    public ArticleForm(String title, String content) {
+        this.title = title;
+        this.content = content;
+    }
+
+    @Override
+    public String toString() {
+        return "ArticleForm{" +
+                "title='" + title + '\'' +
+                ", content='" + content + '\'' +
+                '}';
+    }
+}
