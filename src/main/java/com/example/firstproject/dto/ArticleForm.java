2ce55b6c0f72acf93cafd8b0f7653ae5c10aa0dd
CodeDiary18 <codediary18@gmail.com>
2021-07-23 03:02:46 +0900

    DB with JPA

--- src/main/java/com/example/firstproject/dto/ArticleForm.java
+++ src/main/java/com/example/firstproject/dto/ArticleForm.java
@@ -1,5 +1,7 @@
 package com.example.firstproject.dto;
 
+import com.example.firstproject.entity.Article;
+
 public class ArticleForm {//폼 데이터를 받아올 그릇
     private String title;
     private String content;
@@ -15,5 +17,9 @@ public class ArticleForm {//폼 데이터를 받아올 그릇
                 "title='" + title + '\'' +
                 ", content='" + content + '\'' +
                 '}';
+    }
+
+    public Article toEntity() {
+        return new Article(null,title,content);
     }
 }
