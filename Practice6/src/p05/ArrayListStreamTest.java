7b49eb4fd26fad7c7ac13dc152c9b85fe74a975a
CodeDiary18 <codediary18@gmail.com>
2021-07-23 00:45:51 +0900

    practice5

---
+++ Practice6/src/p05/ArrayListStreamTest.java
@@ -0,0 +1,26 @@
+package p05;
+
+import java.util.ArrayList;
+import java.util.List;
+import java.util.stream.Stream;
+
+public class ArrayListStreamTest {
+
+	public static void main(String[] args) {
+		List<String> sList = new ArrayList<String>();
+		sList.add("Tomas");
+		sList.add("Edward");
+		sList.add("Jack");
+		
+		Stream<String> stream = sList.stream();
+		stream.forEach(s->System.out.print(s + " "));
+		System.out.println();
+		
+		sList.stream().sorted().forEach(s->System.out.print(s+ " "));
+		System.out.println();
+		sList.stream().map(s->s.length()).forEach(n->System.out.print(n+"\t"));
+		System.out.println();
+		sList.stream().filter(s->s.length() >= 5).forEach(s->System.out.println(s));
+	}
+
+}
