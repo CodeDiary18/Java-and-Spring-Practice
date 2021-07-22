ecb9b8c50908c8a98fae4fd4f696ed5975497e85
CodeDiary18 <codediary18@gmail.com>
2021-07-23 03:49:06 +0900

    log 남기기

--- src/main/java/com/example/firstproject/controller/ArticleController.java
+++ src/main/java/com/example/firstproject/controller/ArticleController.java
@@ -3,12 +3,14 @@ package com.example.firstproject.controller;
 import com.example.firstproject.dto.ArticleForm;
 import com.example.firstproject.entity.Article;
 import com.example.firstproject.repository.ArticleRepository;
+import lombok.extern.slf4j.Slf4j;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
 
 @Controller
+@Slf4j  // 로깅을 위한 어노테이션
 public class ArticleController {
 
     @Autowired  //스프링 부트가 미리 생성해놓은 객체를 가져다가 자동 연결!
@@ -21,15 +23,18 @@ public class ArticleController {
 
     @PostMapping("/articles/create")
     public String createArticle(ArticleForm form){
-        System.out.println(form.toString());
+        //System.out.println(form.toString()); -> 로깅으로 대체
+        log.info(form.toString());
 
         // 1. Dto를 Entity로 변환!
         Article article = form.toEntity();
-        System.out.println(article.toString());
+        log.info(article.toString());
+        //System.out.println(article.toString());
 
         // 2. Repository에게 Entity를 DB안에 저장하게 함!
         Article saved = articleRepository.save(article);
-        System.out.println(saved.toString());
+        log.info(article.toString());
+        //System.out.println(saved.toString());
         return "";
     }
 }
