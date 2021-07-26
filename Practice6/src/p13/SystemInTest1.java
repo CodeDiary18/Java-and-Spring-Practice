cddf3b935324adb7cd260ffde2386a125aa6638b
CodeDiary18 <codediary18@gmail.com>
2021-07-27 02:31:42 +0900

    practice13

---
+++ Practice6/src/p13/SystemInTest1.java
@@ -0,0 +1,23 @@
+package p13;
+
+import java.io.IOException;
+import java.io.InputStreamReader;
+
+public class SystemInTest1 {
+
+	public static void main(String[] args) {
+		System.out.println("알파벳 여러 개를 쓰고 [Enter]를 누르세요");
+		int i;
+		
+		try {
+			InputStreamReader irs = new InputStreamReader(System.in);
+			while((i = irs.read())!='\n') {
+//				System.out.println(i);
+				System.out.print((char)i);
+			}
+		} catch (IOException e) {
+			e.printStackTrace();
+		}
+		
+	}
+}
