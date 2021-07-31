a4853ca25210d86b7d9875baf5b142d713727fb4
CodeDiary18 <codediary18@gmail.com>
2021-08-01 06:32:26 +0900

    Object Mapper

---
+++ src/main/java/Main.java
@@ -0,0 +1,57 @@
+import com.fasterxml.jackson.core.JsonProcessingException;
+import com.fasterxml.jackson.core.type.TypeReference;
+import com.fasterxml.jackson.databind.JsonNode;
+import com.fasterxml.jackson.databind.ObjectMapper;
+import com.fasterxml.jackson.databind.node.ArrayNode;
+import com.fasterxml.jackson.databind.node.ObjectNode;
+import dto.Car;
+import dto.User;
+
+import java.util.Arrays;
+import java.util.List;
+
+public class Main {
+    public static void main(String[] args) throws JsonProcessingException {
+        System.out.println("main");
+
+        ObjectMapper objectMapper = new ObjectMapper();
+        User user = new User();
+        user.setName("홍길동");
+        user.setAge(50);
+
+        Car car1 = new Car();
+        car1.setName("k5");
+        car1.setCarNumber("11가 1234");
+        car1.setType("sedan");
+
+        Car car2 = new Car();
+        car2.setName("q5");
+        car2.setCarNumber("22가 1234");
+        car2.setType("suv");
+
+        List<Car> carList = Arrays.asList(car1, car2);
+        user.setCars(carList);
+
+//        System.out.println(user);
+
+        String json = objectMapper.writeValueAsString(user);
+        System.out.println(json);
+
+        JsonNode jsonNode = objectMapper.readTree(json);
+        String _name = jsonNode.get("name").asText();
+        int _age = jsonNode.get("age").asInt();
+        System.out.println("name : " + _name);
+        System.out.println("age : " + _age);
+
+        JsonNode cars = jsonNode.get("cars");
+        ArrayNode arrayNode = (ArrayNode) cars;
+        List<Car> _cars = objectMapper.convertValue(arrayNode, new TypeReference<List<Car>>() {
+        });
+        System.out.println(_cars);
+
+        ObjectNode objectNode = (ObjectNode) jsonNode;
+        objectNode.put("name", "c55");
+        objectNode.put("age", 22);
+        System.out.println(objectNode.toPrettyString());
+    }
+}
