293f8585a38fd8038f1fb494e46d9f68b9119ddb
CodeDiary18 <codediary18@gmail.com>
2021-08-02 05:47:42 +0900

    Validation - @NotBlank, @Min, @Max

--- src/main/java/com/example/validation/controller/ApiController.java
+++ src/main/java/com/example/validation/controller/ApiController.java
@@ -1,7 +1,10 @@
 package com.example.validation.controller;
 
 import com.example.validation.dto.User;
+import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
+import org.springframework.validation.BindingResult;
+import org.springframework.validation.FieldError;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -14,8 +17,22 @@ import javax.validation.Valid;
 public class ApiController {
 
     @PostMapping("/user")
-    public ResponseEntity user(@Valid @RequestBody User user){
-        System.out.println(user);
+    public ResponseEntity user(@Valid @RequestBody User user, BindingResult bindingResult) {
+        if (bindingResult.hasErrors()) {
+            StringBuilder sb = new StringBuilder();
+            bindingResult.getAllErrors().forEach(objectError -> {
+                FieldError field = (FieldError) objectError;
+                String message = objectError.getDefaultMessage();
+//                System.out.println("field : "+field.getField());
+//                System.out.println(message);
+                sb.append("field : " + field.getField() + "\n");
+                sb.append("message : " + message + "\n");
+            });
+            return ResponseEntity.status(HttpStatus.BAD_REQUEST).body(sb.toString());
+        }
+
+        // logic
+
         return ResponseEntity.ok(user);
     }
 }
--- src/main/java/com/example/validation/dto/User.java
+++ src/main/java/com/example/validation/dto/User.java
@@ -2,18 +2,20 @@ package com.example.validation.dto;
 
 import lombok.Data;
 
-import javax.validation.constraints.Email;
-import javax.validation.constraints.Pattern;
+import javax.validation.constraints.*;
 
 @Data
 public class User {
+    @NotBlank
     private String name;
 
+    @Min(value=1)
+    @Max(value=100)
     private int age;
 
     @Email
     private String email;
 
-    @Pattern(regexp = "^\\d{2,3}-\\d{3,4}-\\d{4}$") //핸드폰 정규식
+    @Pattern(regexp = "^\\d{2,3}-\\d{3,4}-\\d{4}$",message = "핸드폰 번호의 양식과 맞지 않습니다. 01x-xxx(x)-xxxx") //핸드폰 정규식
     private String phoneNumber;
 }
