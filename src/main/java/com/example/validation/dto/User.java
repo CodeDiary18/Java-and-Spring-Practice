22f9469b5fb3db284f4f1ea1a9d61ebe2cf65965
CodeDiary18 <codediary18@gmail.com>
2021-08-03 05:58:26 +0900

    Validation - @YearMonth, 날짜 검증 annotation 생성

--- src/main/java/com/example/validation/dto/User.java
+++ src/main/java/com/example/validation/dto/User.java
@@ -1,10 +1,9 @@
 package com.example.validation.dto;
 
+import com.example.validation.annotation.YearMonth;
 import lombok.Data;
 
 import javax.validation.constraints.*;
-import java.time.LocalDate;
-import java.time.format.DateTimeFormatter;
 
 @Data
 public class User {
@@ -21,16 +20,6 @@ public class User {
     @Pattern(regexp = "^\\d{2,3}-\\d{3,4}-\\d{4}$", message = "핸드폰 번호의 양식과 맞지 않습니다. 01x-xxx(x)-xxxx") //핸드폰 정규식
     private String phoneNumber;
 
-    @Size(min = 6, max = 6)
+    @YearMonth
     private String reqYearMonth;    //yyyyMM
-
-    @AssertTrue(message="yyyyMM의 형식에 맞지 않습니다.")
-    public boolean isReqYearMonthValidation() { //앞에 is 붙여야 함
-        try{
-            LocalDate localDate = LocalDate.parse(getReqYearMonth()+"01", DateTimeFormatter.ofPattern("yyyyMMdd"));
-        }catch (Exception e){
-            return false;
-        }
-        return true;
-    }
 }
