ee1ad3b1c6dfe10a9b27a5ba23aa35f82589f016
CodeDiary18 <codediary18@gmail.com>
2021-08-06 04:30:31 +0900

    Async - 비동기 처리

--- src/main/java/com/example/async/AsyncApplication.java
+++ src/main/java/com/example/async/AsyncApplication.java
@@ -2,8 +2,10 @@ package com.example.async;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.scheduling.annotation.EnableAsync;
 
 @SpringBootApplication
+@EnableAsync
 public class AsyncApplication {
 
     public static void main(String[] args) {
