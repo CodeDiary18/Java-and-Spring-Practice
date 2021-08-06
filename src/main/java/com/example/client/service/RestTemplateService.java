82d1b77e9c1d8b7add1fb6eb8610f8fdf7c1da5a
CodeDiary18 <codediary18@gmail.com>
2021-08-07 04:00:34 +0900

    Rest Template - Client Get

---
+++ src/main/java/com/example/client/service/RestTemplateService.java
@@ -0,0 +1,34 @@
+package com.example.client.service;
+
+import com.example.client.dto.UserResponse;
+import org.springframework.http.ResponseEntity;
+import org.springframework.stereotype.Service;
+import org.springframework.web.client.RestTemplate;
+import org.springframework.web.util.UriComponentsBuilder;
+
+import java.net.URI;
+
+@Service
+public class RestTemplateService {
+    // http://localhost/api/server/hello
+    // response
+    public UserResponse hello() {
+        URI uri = UriComponentsBuilder.
+                fromUriString("http://localhost:9090")
+                .path("/api/server/hello")
+                .queryParam("name","cd18")
+                .queryParam("age",10)
+                .encode()
+                .build()
+                .toUri();
+        System.out.println(uri.toString());
+
+        RestTemplate restTemplate = new RestTemplate();
+//        String result = restTemplate.getForObject(uri, String.class);
+        ResponseEntity<UserResponse> result = restTemplate.getForEntity(uri, UserResponse.class);
+
+        System.out.println(result.getStatusCode());
+        System.out.println(result.getBody());
+        return result.getBody();
+    }
+}
