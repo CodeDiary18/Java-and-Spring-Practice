846ee988d9f058650e0820d0a9bafaffdeaeca4e
CodeDiary18 <codediary18@gmail.com>
2021-08-08 01:55:55 +0900

    Rest Template - Server Post

--- src/main/java/com/example/server/controller/ServerApiController.java
+++ src/main/java/com/example/server/controller/ServerApiController.java
@@ -1,15 +1,22 @@
 package com.example.server.controller;
 
 import com.example.server.dto.User;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.web.bind.annotation.*;
 
+@Slf4j
 @RestController
 @RequestMapping("/api/server")
 public class ServerApiController {
     @GetMapping("/hello")
     public User hello(User user) {
+        return user;
+    }
+
+    @PostMapping("/user/{userId}/name/{userName}")
+    public User post(@RequestBody User user, @PathVariable int userId, @PathVariable String userName) {
+        log.info("userId : {}, userName : {}", userId, userName);
+        log.info("client req : {}", user);
         return user;
     }
 }
